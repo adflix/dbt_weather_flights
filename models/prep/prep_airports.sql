6d2c26bf43076843559fb9fd63a36d02cf931c61
Tim Schulz-Eppers <teppers@Tims-MBP-2.fritz.box>
2024-10-14 16:31:23 +0200

    staging models

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,13 @@
+WITH airports_reorder AS (
+    SELECT faa
+           ,name
+           ,city
+           ,state
+           ,country
+           ,lat
+           ,lon
+           ,tz
+    FROM {{ref('staging_airports')}}
+)
+SELECT * 
+FROM airports_reorder;
