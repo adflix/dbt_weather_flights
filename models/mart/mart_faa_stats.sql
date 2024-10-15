6aee4691129a45cbde699c2cf991420c3788a80a
Tim Schulz-Eppers <teppers@Tims-MBP-2.fritz.box>
2024-10-15 14:57:33 +0200

    unnötige kommis entfernt

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,25 +1,24 @@
--- Entferne das "SELECT *" über dem WITH
 WITH departures AS (
     SELECT origin AS faa
            ,COUNT(origin) AS nunique_from 
-           ,COUNT(sched_dep_time) AS dep_planned -- geplante Abflüge
-           ,SUM(cancelled) AS dep_cancelled -- stornierte Abflüge
-           ,SUM(diverted) AS dep_diverted -- umgeleitete Abflüge
-           ,COUNT(arr_time) AS dep_n_flights -- tatsächliche Abflüge
-           ,COUNT(DISTINCT tail_number) AS dep_nunique_tails -- einzigartige Flugzeuge
-           ,COUNT(DISTINCT airline) AS dep_nunique_airlines -- einzigartige Fluglinien
+           ,COUNT(sched_dep_time) AS dep_planned 
+           ,SUM(cancelled) AS dep_cancelled 
+           ,SUM(diverted) AS dep_diverted 
+           ,COUNT(arr_time) AS dep_n_flights 
+           ,COUNT(DISTINCT tail_number) AS dep_nunique_tails 
+           ,COUNT(DISTINCT airline) AS dep_nunique_airlines 
     FROM {{ref('prep_flights')}}
     GROUP BY origin
 ),
 arrivals AS (
     SELECT dest AS faa
            ,COUNT(dest) AS nunique_to 
-           ,COUNT(sched_dep_time) AS arr_planned -- geplante Ankünfte
-           ,SUM(cancelled) AS arr_cancelled -- stornierte Ankünfte
-           ,SUM(diverted) AS arr_diverted -- umgeleitete Ankünfte
-           ,COUNT(arr_time) AS arr_n_flights -- tatsächliche Ankünfte
-           ,COUNT(DISTINCT tail_number) AS arr_nunique_tails -- einzigartige Flugzeuge
-           ,COUNT(DISTINCT airline) AS arr_nunique_airlines -- einzigartige Fluglinien
+           ,COUNT(sched_dep_time) AS arr_planned 
+           ,SUM(cancelled) AS arr_cancelled 
+           ,SUM(diverted) AS arr_diverted 
+           ,COUNT(arr_time) AS arr_n_flights 
+           ,COUNT(DISTINCT tail_number) AS arr_nunique_tails 
+           ,COUNT(DISTINCT airline) AS arr_nunique_airlines 
     FROM {{ref('prep_flights')}}
     GROUP BY dest
 ),
@@ -34,7 +33,7 @@ total_stats AS (
     FROM departures
     JOIN arrivals USING (faa)
 )
--- Füge Stadt, Land und den Flughafennamen hinzu
+-- PLUS Stadt, Land und Name des Flughafens
 SELECT city  
        ,country
        ,name
